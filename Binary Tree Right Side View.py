55bc7c1dbd2bb66726d561b5fe3f8ae3fc84832e
pufferlulu <zytprivate@gmail.com>
2018-10-13 18:47:56 -0700

    LC199

---
+++ Binary Tree Right Side View.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        view = []
+        if root:
+            level = [root]
+            while level:
+                view += level[-1].val,
+                level = [kid for node in level for kid in (node.left, node.right) if kid]
+        return view
