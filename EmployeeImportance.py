d1ecf99302f741f08305fb85d6de5e3d1dc02267
pufferlulu <zytprivate@gmail.com>
2018-10-16 00:41:56 -0700

    LC690

---
+++ EmployeeImportance.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        # Time: O(n)
+        # Space: O(n)
+        emps = {employee.id: employee for employee in employees}
+        def dfs(id):
+            subordinates_importance = sum([dfs(sub_id) for sub_id in emps[id].subordinates])
+            return subordinates_importance + emps[id].importance
+        return dfs(id)
