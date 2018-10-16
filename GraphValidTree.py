9746d832fb20b66c3259d767ce003c374bf78c02
pufferlulu <zytprivate@gmail.com>
2018-10-15 23:32:25 -0700

    LC 261 bfs and union find

--- GraphValidTree.py
+++ GraphValidTree.py
@@ -11,3 +11,25 @@ class Solution(object):
                 return False
             parent[x] = y
         return len(edges) == n - 1
+
+import collections
+class Solution:
+  def validTree(self, n, edges):
+    if len(edges) != n - 1:
+      return False
+
+    neighbors = collections.defaultdict(list)
+    for k, v in edges:
+      neighbors[k].append(v)
+      neighbors[v].append(k)
+
+    visited = []
+    queue = [0]
+    while queue:
+      cur = queue.pop()
+      visited.append(cur)
+      queue += [node for node in neighbors[cur] if node not in visited]
+  
+    return len(visited) == n
+    
+    
