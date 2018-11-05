12476e5c822b8df01d161aba7360436a498f9905
pufferlulu <zytprivate@gmail.com>
2018-11-04 23:06:38 -0800

    LC78

---
+++ Subset.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def subsets(self, nums):
+        def backtrack(start, end, tmp):
+            ans.append(tmp[:])
+            for i in range(start, end):
+                tmp.append(nums[i])
+                backtrack(i+1, end, tmp)
+                tmp.pop()
+        ans = []
+        backtrack(0, len(nums), [])
+        return ans
