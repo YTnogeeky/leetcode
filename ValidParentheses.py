5eeb75c98cbab121e05cd52bd805bf5535805098
pufferlulu <zytprivate@gmail.com>
2018-11-04 23:07:38 -0800

    LC20

---
+++ ValidParentheses.py
@@ -0,0 +1,12 @@
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+        return stack == []
