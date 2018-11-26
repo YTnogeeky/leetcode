572516b6eb87f0a0275e69eea6a3589c3a76186a
pufferlulu <zytprivate@gmail.com>
2018-11-25 18:31:32 -0800

    LC636

---
+++ Exclusive Time of Functions.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    @param n: a integer
+    @param logs: a list of integers
+    @return: return a list of integers
+    """
+    def exclusiveTime(self, n, logs):
+        stack = []
+        result = [0 for i in range(n)]
+        
+        last_timestamp = 0
+        for str in logs:
+            log = str.split(':')
+            id, status, timestamp = int(log[0]), log[1], int(log[2])
+            if status == 'start':
+                if stack:
+                    result[stack[-1]] += timestamp - last_timestamp
+                stack.append(id)
+            
+            else:
+                timestamp += 1
+                result[stack.pop()] += timestamp - last_timestamp
+            last_timestamp = timestamp 
+        return result
